9c8d815868d05aac7daebfe7fa43f9b2497859f3
Dumitru <119630796+DiacencoDumitru@users.noreply.github.com>
2026-03-15 14:55:44 +0200

    chore: migrate project to Spring Boot

---
+++ src/main/java/org/example/ActivityLogApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
+
+@SpringBootApplication(exclude = CassandraAutoConfiguration.class)
+public class ActivityLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityLogApplication.class, args);
+    }
+}
